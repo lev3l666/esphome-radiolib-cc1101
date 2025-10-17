0ec6b607f593bb6cdabf46ac74f66fa0897195ea
Jairo Sierra <jairo.sierra@brandlive.com>
2025-10-17 12:21:04 -0500

    Register the "on_packet" automation

--- components/radiolib_cc1101/__init__.py
+++ components/radiolib_cc1101/__init__.py
@@ -16,12 +16,19 @@ RadiolibCC1101Component = radiolib_cc1101_ns.class_(
     "RadiolibCC1101Component", cg.Component, spi.SPIDevice
 )
 
+# --- Add a trigger for "on_packet" ---
+RadiolibCC1101PacketTrigger = radiolib_cc1101_ns.class_(
+    "RadiolibCC1101PacketTrigger", cg.Trigger.template()
+)
+
 CONF_MODULATION = "modulation"
 CC1101Modulation = radiolib_cc1101_ns.enum("CC1101Modulation")
 CC1101_MODULATIONS = {
     "OOK": CC1101Modulation.OOK_MODULATION,
     "FSK": CC1101Modulation.FSK_MODULATION,
 }
+
+CONF_ON_PACKET = "on_packet"
 
 CONFIG_SCHEMA = (
     cv.Schema({
@@ -30,10 +37,11 @@ CONFIG_SCHEMA = (
         cv.Optional(CONF_FREQUENCY, default="433.92MHz"): cv.frequency,
         cv.Optional(CONF_MODULATION, default="ook"): cv.enum(CC1101_MODULATIONS, upper=True, space="_"),
         cv.Optional(CONF_FILTER, default="464kHz"): cv.frequency,
-        cv.Optional('bitrate',default=5): cv.float_range(0.025,600), # 40k = 25us resolution, 5k=less noisy
+        cv.Optional('bitrate',default=5): cv.float_range(0.025,600),
         cv.Optional('reg_agcctrl0',default=0xb2): cv.hex_uint8_t,
         cv.Optional('reg_agcctrl1',default=0x00): cv.hex_uint8_t,
         cv.Optional('reg_agcctrl2',default=0xc7): cv.hex_uint8_t,
+        cv.Optional(CONF_ON_PACKET): automation.validate_automation(single=True),
         })
     .extend(cv.COMPONENT_SCHEMA)
     .extend(spi.spi_device_schema(cs_pin_required=True))
@@ -41,18 +49,11 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
 
-    # When using Arduino, RadioLib includes SPI.h (even though we abstract out the SPI functions)
-    # so we need RadioLib to be able to find it...
     if CORE.using_arduino:
-        # This used to work ... but then deep+ got broken... https://github.com/esphome/esphome/issues/9672
-        #cg.add_library("SPI",None)        
-        #cg.add_platformio_option("lib_ldf_mode", "deep+")
-
-        # so now we do this...
         prescript=os.path.join(os.path.dirname(__file__), "hack_radiolib_buildopt.py")
         cg.add_platformio_option("extra_scripts", [f"pre:{prescript}"])
 
-    cg.add_library("RadioLib",None)
+    cg.add_library("RadioLib", None)
 
     var = cg.new_Pvariable(config[CONF_ID])
 
@@ -67,6 +68,14 @@ async def to_code(config):
     cg.add(var.set_reg_agcctrl0(config['reg_agcctrl0']))
     cg.add(var.set_reg_agcctrl1(config['reg_agcctrl1']))
     cg.add(var.set_reg_agcctrl2(config['reg_agcctrl2']))
+
+    # Register the "on_packet" automation
+    if CONF_ON_PACKET in config:
+        trigger = cg.new_Pvariable(config[CONF_ON_PACKET][CONF_TRIGGER_ID])
+        await cg.register_component(trigger, config)
+        await automation.build_automation(trigger, [], config[CONF_ON_PACKET])
+
+        cg.add(var.set_on_packet_trigger(trigger))
 
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
